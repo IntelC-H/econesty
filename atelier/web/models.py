8cbabeb632c2d54ae34bcbce9c44a72e0b141eef
IntelCore0607 <mullanebenjamin17@gmail.com>
2017-05-24 16:07:39 -0400

    More models

--- atelier/web/models.py
+++ atelier/web/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# IMPORTANT: Atelier could be a place for normal people to find where
+#            their favorite artists are being shown.
+
+class Appraisal(models.Model):
+  value = models.DecimalField(max_digits=11, decimal_places=2)
+  value_currency = models.CharField(max_length=3)
+  date = models.DateTimeField(default=timezone.now())
+  appraiser_name = models.TextField()
+  appraiser_phone = models.TextField()
+  appraiser_email = models.TextField()
+  signature_svg = models.TextField()
+
+# This is a piece of art
+class Artwork(models.Model):
+  media = models.TextField()
+  artist_name = models.TextField()
+  owner = models.ForeignKey(Owner, on_delete=models.CASCADE) # TODO: make sure this relationship is accurate.
+  image_url = models.TextField() # is there a URL field in Django?
+  thumb_url = models.TextField() # is there a URL field in Django?
+
+# This is the model representing artwork owners, the "users" of Atelier.
+class Owner(models.Model):
+  name = models.TextField()
+  phone_number = models.CharField(max_length=50)
+  address = models.TextField()
+  email = models.TextField()
+  avatar_url = models.TextField()
+  thumb_url = models.TextField()
+
+# Where a
+class Location(models.Model):
+  owner = models.ForeignKey(Owner, on_delete=models.CASCADE) # TODO: make sure this relationship is accurate
+  name = models.TextField()
+  phone_number = models.CharField(max_length=50)
+  email = models.TextField()
+  address = models.TextField()
+  lat = models.DecimalField(max_digits=3, decimal_places=8)
+  lon = models.DecimalField(max_digits=3, decimal_places=8)
+ 
