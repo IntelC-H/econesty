38ab05454e2264fb803f6492c14241d5ca9eca5b
IntelCore0607 <mullanebenjamin17@gmail.com>
2017-05-31 10:53:31 -0400

    Improve correctness of models

--- atelier/web/models.py
+++ atelier/web/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+# TODO: mark deleted instead of deleting.
+
 # IMPORTANT: Atelier could be a place for normal people to find where
 #            their favorite artists are being shown.
 
@@ -16,16 +18,19 @@ class Appraisal(models.Model):
   signature_svg = models.TextField()
 
 class Artwork(models.Model):
-  artist_name = models.TextField()
-  description = models.TextField()
-  media = models.TextField()
+  artist_name = models.TextField(null=True)
+  description = models.TextField(null=True)
+  media = models.TextField(null=True) # TODO: should this be done with Django's @choices@ field option?
   image_url = models.URLField()
   thumb_url = models.URLField()
 
 class Ownership(models.Model):
   start_date = models.DateTimeField(default=timezone.now())
-  end_date = models.DateTimeField(default=tiimezone.now())
+  end_date = models.DateTimeField(null=True)
   artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
+
+  def is_current(self):
+    return self.end_date == None
 
 class Person(models.Model):
   name = models.TextField()
@@ -44,8 +49,41 @@ class Location(models.Model):
   from_date = models.DateTimeField(default=timezone.now())
   until_date = models.DateTimeField()
 
+  def is_temporary(self):
+    return (self.from_date != None and self.until_date != None)
+
 class User(models.Model):
+  ARTIST = "ar"
+  CONNOISSEUR = "cn"
   username = models.TextField()
+  kind = models.CharField(max_length=2, default=CONNOISSEUR, choices=((ARTIST, "Artist"),(CONNOISSEUR, "Connoisseur")))
   password_hash = models.TextField()
   owner = models.ForeignKey(Owner, on_delete=models.RESTRICT) # TODO: check this
+
+  def check_password(self, password):
+    return False
+
+  def is_artist(self):
+    return self.kind == self.ARTIST
+
+  def is_connoisseur(self):
+    return self.kind == self.CONNOISSEUR
+
+# TODO: this needs to be implemented better so including encrypted data works.
+class PaymentMethod(models.Model):
+  slug = models.TextField()
+  name = models.TextField()
+
+class Transaction(models.Model):
+  artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
+  buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+  seller = models.ForeignKey(User, on_delete=models.CASCADE)
+  required_witnesses = models.IntegerField(default=0)
+  payment_method = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE)
+  offer = models.DecimalField(max_digits=11, decimal_places=2)
+  offer_currency = models.CharField(max_length=3)
+
+class Witnessing(models.Model):
+  witness = models.ForeignKey(User, on_delete=models.CASCADE)
+  transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
 
