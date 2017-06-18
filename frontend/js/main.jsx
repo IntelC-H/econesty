8264dc945fd713f8cc7a7081fe3f06f5e8fa604f
IntelCore0607 <mullanebenjamin17@gmail.com>
2017-06-18 12:19:04 -0400

    More pages

--- frontend/js/main.jsx
+++ frontend/js/main.jsx
@@ -1,29 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Switch, Route } from 'react-router';
-import RESTObject from 'app/restobject';
+import { Switch, Route, browserHistory } from 'react-router';
+import { RESTComponent, RESTModel } from 'app/restobject';
 
 var container = document.getElementById("main");
 
-// Links to other pages
-// <Link to="/">Home</Link>
+// TODO: caching using browser sessionStorage
 
-// e.g.: navigateTo("/")
-// const navigateTo = React.PropTypes.object.push
-//
-//
-// class HomePage extends React.Component {
-//   render() {
-//     return <p>HOME</p>
-//   }
-// }
+// Login
 
-class ProfilePage extends React.Component {
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
   render() {
-    return (<RESTObject
+    return (
+      <div>
+        <input type="text" onChange={(e) => this.handleUsernameChange(e)} />
+        <input type="password" onChange={(e) => this.handlePasswordChange(e)} />
+        <button onClick={() => this.handleLogin()}>Login</button>
+      </div>
+    );
+  }
+ 
+  handleLogin() {
+    RESTModel.authenticate(this.state.username, this.state.password, () => {
+      this.props.history.push("/user/me");
+    });
+  }
+
+  handleUsernameChange(e) {
+    e.persist();
+    this.setState((st) => {
+      return { username: e.target.value, password: st.password };
+    });
+  }
+  
+  handlePasswordChange(e) {
+    e.persist();
+    this.setState((st) => {
+      return { username: st.username, password: e.target.value };
+    });
+  }
+}
+
+// HOME
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>HOME</p>
+        <button onClick={(_) => this.props.history.push("/user/me")}>Profile</button>
+      </div>
+    );
+  }
+}
+
+class Profile extends React.Component {
+  render() {
+    return (<RESTComponent
             resource="user"
-            objectId="2"
+            object={this.props.match.params.user}
             component={UserRepresentation}/>);
   }
 }
@@ -31,56 +78,28 @@ class ProfilePage extends React.Component {
 class UserRepresentation extends React.Component {
   render() {
     var model = this.props.model;
-    if (model.error != null) {
-      return <h1>Error: {model.error}</h1>;
-    } else if (model.object != null) {
-      var user = model.object;
-      return <h1>Welcome, @{user.username}!</h1>;
+    if (model.isPersisted) {
+      if (model.error != null) {
+        return <h1>Error: {model.error}</h1>;
+      } else if (model.object != null) {
+        var user = model.object;
+        return <h1>Welcome, @{user.username}!</h1>;
+      } else {
+        return <h3></h3>;
+      }
     } else {
-      return <h3></h3>;
+      return <h1>Signup Needed</h1>;
     } 
   }
 }
 
-// class SignupPage extends React.Component {
-//   render() {
-//     return <p>SIGNUP</P>
-//   }
-// }
-//
-// class PaymentDataPage extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       paymentData: null
-//     }
-//   }
-//   render() {
-//     return <p>PAYMENTDATA</p>
-//   }
-// }
-//
-// class CounterSignaturePage extends React.Component {
-//   render() {
-//     return <p>PaymentData</p>
-//   }
-// }
-
-      /* <Route exact path='/' component={}> // homepage
-       <Route exact path='/user' component={ProfilePage}> // logged in user
-       <Route exact path='/user/new' component={SignupPage}> // create user */
-
 ReactDOM.render((
    <BrowserRouter>
      <div>
-       <Route path='/user/:id' component={ProfilePage} />
+       <Route exact path="/" component={HomePage} />
+       <Route exact path="/login" component={Login} />
+       <Route exact path="/signup" component={Profile} />
+       <Route path='/user/:user' component={Profile} />
      </div>
    </BrowserRouter>
 ), container);
-
-
-      // <Route exact path='/paymentdata/new' component={PaymentDataPage}>
-      // <Route exact path='/paymentdata/:id' component={PaymentDataPage}>
-      // <Route exact path='/transaction/new' component={TransactionPage}>
-      // <Route path='/transaction/:id' component={TransactionPage}>
-      // <Route path='/transaction/:id/countersign' component={CounterSignaturePage}>
--- frontend/js/restobject.jsx
+++ frontend/js/restobject.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 
-export class RESTModel {
+function baseURL() {
+  return window.location.protocol + "//" + window.location.host + "/api/"; 
+}
+
+class RESTModel {
   constructor(rsc, callb = null) {
     this.resource = rsc;
     this.callback = callb;
@@ -16,8 +20,8 @@ export class RESTModel {
     return this.objectId != null;
   }
 
-  load(objectId=this.objectId) {
-    this.fetchJSON(this.getURL(objectId), {});
+  load(rmeth=this.objectId) {
+    this.fetchJSON(this.getURL(rmeth), {});
   }
 
   save() {
@@ -34,13 +38,36 @@ export class RESTModel {
     }
   }
 
-  getURL(id = this.objectId) {
-    var u = window.location.protocol + "//" + window.location.host + "/api/" + this.resource + "/";
-    if (id != null) {
-      u += id + "/";
+  getURL(rmeth = this.objectId) {
+    var u = baseURL() + this.resource + "/";
+    if (rmeth != null) {
+      u += rmeth + "/";
     }
     u += "?format=json";
     return u;
+  }
+
+  static authenticate(username, password, callback) {
+    var body = { username: username, password: password };
+    fetch(baseURL() + "token/", {method: "POST", body: JSON.stringify(body), headers: {"Content-Type": "application/json"}}).then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        var error = new Error(res.statusText);
+        error.response = res;
+        throw error;
+      }
+    }).then((json) => {
+      localStorage.setItem("token", json.token);
+      callback(null);
+    }).catch((err) => {
+      callback(err);
+    });
+  }
+
+  static isAuthenticated() {
+    var t = localStorage.getItem("token");
+    return t != null && t != undefined;
   }
 
   fetchJSON(url, opts, callback) {
@@ -50,10 +77,17 @@ export class RESTModel {
     }
 
     var optsp = opts;
+    optsp.headers = {}
     if (optsp.json != undefined) {
       optsp.body = JSON.stringify(optsp.json);
       optsp.headers["Content-Type"] = "application/json";
     }
+
+    var tok = localStorage.getItem("token");
+    if (tok != null && tok != undefined) {
+      optsp.headers.Authorization = "Token " + tok;
+    }
+
     optsp.method = optsp.method || "GET"; 
 
     fetch(url, optsp).then((res) => {
@@ -82,28 +116,39 @@ export class RESTModel {
   }
 }
 
-export default class RESTComponent extends React.Component {
+class RESTComponent extends React.Component {
   constructor(props) {
     super(props);
-    var m = new RESTModel(this.props.resource, ((x) => this.updateWithModel(x)));
-    m.objectId = this.props.objectId;
-    this.state = { model: m }
     this.setState = this.setState.bind(this);
+    this.updateWithModel = this.updateWithModel.bind(this);
+
+    var m = new RESTModel(this.props.resource, this.updateWithModel);
+    if (!isNaN(this.props.object)) {
+      m.objectId = this.props.object;
+    }
+
+    this.state = { model: m };
   }
 
   updateWithModel(m) {
-    console.log(this);
+    if (m.object != null && m.object != undefined) {
+      m.objectId = m.object.id;
+    }
     this.setState({model: m});
   }
 
   componentDidMount() {
-    if (this.state.model.isPersisted) {
-      this.state.model.load();
+    var m = this.state.model;
+    if (m.isPersisted) {
+      m.load();
+    } else if (this.props.object != undefined && this.props.object != null) {
+      m.load(this.props.object);
     }
   }
 
   render() {
-    return new this.props.component({model: this.state.model}).render();
+    return React.createElement(this.props.component, {model: this.state.model}, null);
   }
 }
 
+export { RESTComponent, RESTModel }
