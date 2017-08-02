1d0b969eeda02612168676da9bc8caefc7a61953
IntelCore0607 <mullanebenjamin17@gmail.com>
2017-08-02 15:31:59 -0400

    Router optimizations

--- frontend/js/routing.jsx
+++ frontend/js/routing.jsx
@@ -1,6 +1,13 @@
 import { h, Component, cloneElement } from 'preact';
 
+/*
+  Supports the same API as preact-router.
+  Router checks each of its children with a path
+  attribute: if it matches the current URL, it renders it.
+*/
+
 const subscribers = [];
+const updateSubscribers = url => subscribers.forEach(s => s(url))
 
 class Router extends Component {
   constructor(props) {
@@ -25,14 +32,16 @@ class Router extends Component {
     var routes = this.props.children;
     for (var i = 0; i < routes.length; i++) {
       var c = routes[i];
-      var matches = this.test(c.attributes.path, this.state.url);
-      if (matches) {
-        return cloneElement(c, {
-          matches: matches,
-          url: this.state.url,
-          path: c.attributes.path,
-          ...matches
-        });
+      if (c.attributes && c.attributes.path) {
+        var matches = this.test(c.attributes.path, this.state.url);
+        if (matches) {
+          return cloneElement(c, {
+            matches: matches,
+            url: this.state.url,
+            path: c.attributes.path,
+            ...matches
+          });
+        }
       }
     }
     return null;
@@ -42,9 +51,7 @@ class Router extends Component {
     var urlpath_comps = url.replace(/\?.+$/, '').split('/').filter(e => e.length > 0);
     var path_comps = path.split('/').filter(e => e.length > 0);
 
-    if (path_comps.length !== urlpath_comps.length) {
-      return false;
-    }
+    if (path_comps.length !== urlpath_comps.length) return false;
 
     var matches = {};
 
@@ -52,11 +59,8 @@ class Router extends Component {
       var upc = urlpath_comps[i];
       var pc = path_comps[i];
 
-      if (pc.startsWith(':')) {
-        matches[pc.slice(1)] = upc;
-      } else if (pc !== upc) {
-        return false;
-      }
+      if (pc.startsWith(':')) matches[pc.slice(1)] = upc;
+      else if (pc !== upc)    return false;
     }
 
     return matches;
@@ -65,22 +69,16 @@ class Router extends Component {
 
 Router.push = url => {
   history.pushState(null, null, url);
-  for (var i = 0; i < subscribers.length; i++) {
-    subscribers[i](url);
-  }
+  updateSubscribers(url);
 }
 
 Router.replace = url => {
   history.replaceState(null, null, url);
-  for (var i = 0; i < subscribers.length; i++) {
-    subscribers[i](url);
-  }
+  updateSubscribers(url);
 }
 
 window.onpopstate = function(event) {
-  for (var i = 0; i < subscribers.length; i++) {
-    subscribers[i](document.location.pathname);
-  }
+  updateSubscribers(document.location.pathname);
 }
 
 export default Router;
